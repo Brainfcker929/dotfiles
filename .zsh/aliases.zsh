03e40c920940a1dec08114388b4c726fe8cc716f
Paul Rusch <pauly929@hotmail.de>
2024-01-26 09:30:04 +0100

    clean up old linux alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -9,9 +9,7 @@ alias la='ls -A'
 alias l='ls -CF'
 alias ..='cd ..'
 alias ...='cd ../..'
-alias tree='pstree -p'
 alias s='sudo'
-alias linuxversion='cat /etc/os-release'
 alias c='clear'
 alias myip="curl http://ipecho.net/plain; echo"
 alias nis="npm install --save "
@@ -26,6 +24,3 @@ alias push='git push'
 alias pull='git pull'
 alias config='/usr/bin/git --git-dir=/$HOME/.cfg/ --work-tree=/$HOME'
 alias cst='config status'
-
-# Terraform
-alias tf='terraform'

97b196baa10c5ca4b5919a3e07f5db30813ea281
Paul Rusch <pauly929@hotmail.de>
2024-01-27 22:20:37 +0100

    add brew_save alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,8 +1,9 @@
-# Power related commands
-alias shutdown="sudo shutdown -P now"
-alias reboot="sudo shutdown -r now"
+# Power related commands #
+alias shutdown='sudo shutdown -P now'
+alias reboot='sudo shutdown -r now'
+##########################
 
-# general commands
+# General commands #
 alias ls='ls -la --color=auto'
 alias ll='ls -alF'
 alias la='ls -A'
@@ -11,16 +12,22 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias s='sudo'
 alias c='clear'
-alias myip="curl http://ipecho.net/plain; echo"
-alias nis="npm install --save "
-alias mkcd='foo(){ mkdir -p "$1"; cd "$1" }; foo '
-alias df="df -h"
-alias root="sudo su"
-alias cw="code .vscode/project.code-workspace"
+alias myip='curl http://ipecho.net/plain; echo'
+alias nis='npm install --save '
+alias mkcd='foo(){ mkdir -p '$1'; cd '$1' }; foo '
+alias df='df -h'
+alias root='sudo su'
+alias cw='code .vscode/project.code-workspace'
+#####################
 
-# Git commands
+# Git commands #
 alias gst='git status'
 alias push='git push'
 alias pull='git pull'
 alias config='/usr/bin/git --git-dir=/$HOME/.cfg/ --work-tree=/$HOME'
 alias cst='config status'
+################
+
+# Homebrew #
+alias brew_save='brew list --formula > "$BREW_PACKAGE_LIST" && brew list --cask > "$BREW_CASK_LIST"'
+############
